01d776c76949ef891035a81e575e6f276f34465a
Bernardo Carlos Camera <bernardo.camera@dicsys.com>
2025-12-07 00:36:53 -0300

    Arreglé error Unexpected end of JSON y limpié index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+import express from "express";
 import dotenv from "dotenv";
-import app from "./src/app.js";
+
+import productRoutes from "./src/routes/productRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/products", productRoutes);
+app.use("/auth", authRoutes);
+
+app.get("/", (req,res)=>{
+    res.send("API OK");
+});
 
 export default app;
